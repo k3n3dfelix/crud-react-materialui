2d1004d78d8a8c6c5c5e0a013ecc28060256fcff
kenedfelix <kened.felix@gmail.com>
2022-12-16 20:31:20 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppRoutes } from './routes';
 import { MenuLateral } from './shared/components';
 import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 
+
 export const App = () => {
   return (
     <AppThemeProvider>
