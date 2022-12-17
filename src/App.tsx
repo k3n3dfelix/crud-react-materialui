42d22749b56b48570a918bfea6a4842743e50a90
Kened Silva Felix <ksfelix1@stefanini.com>
2022-12-17 17:05:04 -0300

    Barra de ferramentas listagem

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { AppRoutes } from './routes';
 import { MenuLateral } from './shared/components';
 import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 
-
 export const App = () => {
   return (
     <AppThemeProvider>
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,16 @@
+import { BarradeFerramentas } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const DashBoard = () => {
   return (
     <LayoutBaseDePagina
       titulo="Página Inicial"
-      barraDeFerramentas={<>Barra de Ferramentas</>}
+      barraDeFerramentas={(
+        <BarradeFerramentas
+          mostrarInputBusca
+          textoBotaoNovo='Nova'
+        />
+      )}
     >
       Testando
     </LayoutBaseDePagina>
