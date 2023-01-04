5496fd82a29984cc44c48470b9f02198df27474a
Kened Silva Felix <ksfelix1@stefanini.com>
2023-01-03 23:02:16 -0300

    finalizado autoComplete de Cidades

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -171,7 +171,7 @@ export const DetalheDePessoas: React.FC = () => {
             </Grid>
             <Grid container item direction="row">
               <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
-                <AutoCompleteCidade/>
+                <AutoCompleteCidade isExternalLoading={isLoading}/>
               </Grid>
             </Grid>
           </Grid>
--- src/pages/pessoas/components/AutoCompleteCidade.tsx
+++ src/pages/pessoas/components/AutoCompleteCidade.tsx
@@ -1,22 +1,42 @@
 import { useEffect, useMemo, useState } from 'react';
 import { Autocomplete, CircularProgress, TextField } from '@mui/material';
 import { CidadesService } from '../../../shared/services/api/cidades/CidadesService';
+import { useField } from '@unform/core';
 
 type TAutoCompleteOption = {
   id: number;
   label:string;
 }
 
-export const AutoCompleteCidade: React.FC = ( )=> {
+interface IAutoCompleteCidadeProps {
+  isExternalLoading?: boolean;
+}
+export const AutoCompleteCidade: React.FC<IAutoCompleteCidadeProps> = ({ isExternalLoading = false})=> {
+  const { fieldName, registerField, defaultValue, error, clearError } = useField('cidadeId');
+  
   const [options,setOptions ] = useState<TAutoCompleteOption[]>([]);
-  const [selectedId, setSelectedId] = useState<number | undefined>(undefined);
+  const [selectedId, setSelectedId] = useState<number | undefined>(defaultValue);
 
   const [isLoading, setIsLoading] = useState(false);
   const [busca, setBusca] = useState('');
   
   const autoCompleteSelectedOption = useMemo(() => {
+    if (!selectedId) return null;
 
-  }, []);
+    const selectedOption = options.find(option => option.id === selectedId);
+    if (!selectedId) return null;
+
+    return selectedOption;
+  }, [selectedId, options]);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      getValue: () => selectedId,
+      setValue: (_, newSelectedId) => setSelectedId(newSelectedId),
+    });
+  }, [registerField, fieldName, selectedId]);
+  
   useEffect(() => {
     setIsLoading(true);
     CidadesService.getAll(1, busca).then((result) => {
@@ -33,16 +53,27 @@ export const AutoCompleteCidade: React.FC = ( )=> {
 
   return (
     <Autocomplete
-      value={{}}
+      openText='Abrir'
+      closeText='Fechar'
+      clearText='Limpar'
+      noOptionsText="Sem Opçoes"
+      loadingText="Carregando..."
+
+      disablePortal
+
+      value={autoCompleteSelectedOption}
       loading={isLoading}
-      popupIcon={isLoading ? <CircularProgress size={28}/> : undefined}
-      onChange={(_, newValue) => { setSelectedId(newValue?.id); setBusca('');}}
+      disabled={isExternalLoading}
+      popupIcon={(isExternalLoading || isLoading) ? <CircularProgress size={28}/> : undefined}
+      onChange={(_, newValue) => { setSelectedId(newValue?.id); setBusca(''); clearError();}}
       onInputChange={(_, newValue) => setBusca(newValue)}
       options={options}
       renderInput={(params) => (
         <TextField
           {...params}
           label="Cidade"
+          error={!!error}
+          helperText={error}
         />)}
     />
   );
