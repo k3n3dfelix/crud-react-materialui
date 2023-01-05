dc247e33db93f98283469e3a5354136999230dfb
Kened Silva Felix <ksfelix1@stefanini.com>
2023-01-05 10:52:32 -0300

    Criando Service de autentição

---
+++ src/shared/services/api/auth/AuthService.ts
@@ -0,0 +1,26 @@
+import { Api } from '../axios-config';
+
+interface IAuth {
+  accessToken: string;
+}
+
+const auth = async (
+  email: string,
+  password: string
+): Promise<IAuth | Error> => {
+  try {
+    const { data } = await Api.get('/auth', { data: { email, password } });
+
+    if (data) {
+      return data.id;
+    }
+    return new Error('Erro no login.');
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'rro no login.');
+  }
+};
+
+export const AuthService = {
+  auth,
+};
